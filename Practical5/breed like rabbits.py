9f476ecaf74aac4adefeab9684b41f6d67409daf
沈诺 <shennuo@snsndeMacBook-Pro.local>
2023-03-14 23:41:40 +0800

    breed rabbit

--- Practical5/breed like rabbits.py
+++ Practical5/breed like rabbits.py
@@ -1,8 +1,11 @@
 n = 1
 j = 0
+#Limit range to stop the loop at greater than 100
 while j <= 100:
     n = n+1
+#adding gernerations
     j = 2**n
+#Calculating the number of next generations
     print(j)
 print(str(n))
 #the 7 generations exceed 100 rabbits
